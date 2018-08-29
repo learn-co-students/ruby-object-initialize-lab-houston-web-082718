70685b99d1a631e6ebab3583686c4a4f554bcc69
Naeblis <johnmlowery@gmail.com>
2018-08-29 11:30:33 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+
+def initialize(name, breed='Mutt')
+    @name = name
+    @breed = breed
+  end
+
+
+
+end
+
+
+floyd = Dog.new('Floyd', 'Mutt')
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+require 'pry'
+
+class Person
+
+  def initialize(name)
+    @name = name
+
+  end
+end
+
+beyonce = Person.new("Beyonce")
